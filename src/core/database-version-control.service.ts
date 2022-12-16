954f7e14649616becc3966f28ee11d916131d3cb
levpolshkov <polshkov.lv@gmail.com>
2022-12-16 06:36:27 -0600

    Add the sql that allows to build a textual nodes back

--- src/core/database-version-control.service.ts
+++ src/core/database-version-control.service.ts
@@ -1,24 +1,23 @@
-import { Injectable } from "@nestjs/common"
-import { readFileSync } from "fs"
-import { PostgresService } from "./postgres.service"
+import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { PostgresService } from './postgres.service';
 
 @Injectable()
 export class DatabaseVersionControlService {
-  constructor(
-    private pg: PostgresService,
-  ) {
-    console.log("upserting graph procedures")
-    this.upsert_procedures()
+  constructor(private pg: PostgresService) {
+    console.log('upserting graph procedures');
+    this.upsert_procedures();
   }
 
   async upsert_procedures() {
-    await this.runSqlFile('./src/core/sql/graph_create_or_read_word.sql')
-    await this.runSqlFile('./src/core/sql/graph_create_word_sequence.sql')
+    await this.runSqlFile('./src/core/sql/graph_create_or_read_word.sql');
+    await this.runSqlFile('./src/core/sql/graph_create_word_sequence.sql');
+    await this.runSqlFile('./src/core/sql/graph_build_textual_nodes.sql');
   }
 
   async runSqlFile(path: string) {
-    console.log('loading SQL:', path)
+    console.log('loading SQL:', path);
     const data = readFileSync(path, 'utf8');
-    const res = await this.pg.pool.query(data, [])
+    const res = await this.pg.pool.query(data, []);
   }
 }
---
+++ src/core/sql/graph_build_textual_nodes.sql
@@ -0,0 +1,69 @@
+create or replace function graph_build_textual_nodes()
+returns table(id bigint, type varchar(32), properties jsonb, relationships json, value text)
+language sql
+as $$
+	with doc_nodes as (
+		select
+			n.node_id as id,
+			n.node_type as type,
+			coalesce(
+				jsonb_object_agg(npk.property_key, npv.property_value->>'value')
+				filter (where npk is not null),
+				'{}'
+			) as properties,
+			coalesce(
+				(
+					select json_object_agg(grouped.key, grouped.values) as merged
+					from (
+						select rows.key, json_agg(rows.value) as values
+						from (
+							select *
+							from json_each(
+								coalesce(
+									json_object_agg(r.relationship_type, r.to_node_id)
+									filter (where r is not null),
+									'{}'
+								)
+							)
+						) as rows
+						group by rows.key
+					) as grouped
+				),
+				'{}'
+			) as relationships
+		from nodes as n
+		left join node_property_keys as npk on n.node_id=npk.node_id
+		left join node_property_values as npv on npk.node_property_key_id=npv.node_property_key_id
+		left join relationships as r on r.from_node_id=n.node_id
+		left join relationship_property_keys as rpk on rpk.relationship_id=r.relationship_id
+		left join relationship_property_values as rpv on rpv.relationship_property_key_id=rpk.relationship_property_key_id
+		group by n.node_id, n.node_type
+	)
+
+	select
+		dn.*,
+		case dn.type
+			when 'word' then dn.properties->>'word_name'
+			when 'word-sequence'
+			then (
+				select string_agg(words.value::text, ' ') as words
+				from (
+					select npv.property_value->>'value' as value
+					from (
+            select row_number() over() - 1 as index, *
+						from (
+							select json_array_elements(
+								dn.relationships->'word-sequence-to-word'
+							)::text::int as value
+						) as unindexed_node_ids
+          ) as node_ids
+					join nodes as n on n.node_id=value and n.node_type='word'
+					join node_property_keys as npk on n.node_id=npk.node_id and npk.property_key='word_name'
+					join node_property_values as npv on npk.node_property_key_id=npv.node_property_key_id
+					order by node_ids.index asc
+				) as words
+			)
+			else '-'
+		end as value
+	from doc_nodes as dn
+$$;
