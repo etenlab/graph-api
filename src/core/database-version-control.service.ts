6d387bb330f86255579553d361c8a6f1746636ab
levpolshkov <polshkov.lv@gmail.com>
2022-12-29 14:06:15 -0600

    Add test of building verses back

--- src/core/database-version-control.service.ts
+++ src/core/database-version-control.service.ts
@@ -2,6 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { readFileSync } from 'fs';
 import { PostgresService } from './postgres.service';
 
+export const sqlFilePaths = [
+  './src/core/sql/graph_create_or_read_word.sql',
+  './src/core/sql/graph_create_word_sequence.sql',
+  './src/core/sql/graph_create_or_read_word_sequence.sql',
+  './src/core/sql/graph_build_textual_nodes.sql',
+  './src/core/sql/graph_utils.sql',
+  './src/core/sql/graph_add_relationship.sql',
+  './src/core/sql/graph_add_node.sql',
+  './src/core/sql/graph_create_verse.sql',
+  './src/core/sql/graph_create_chapter.sql',
+  './src/core/sql/graph_create_book.sql',
+  './src/core/sql/graph_create_bible.sql',
+];
+
 @Injectable()
 export class DatabaseVersionControlService {
   constructor(private pg: PostgresService) {
@@ -10,24 +24,14 @@ export class DatabaseVersionControlService {
   }
 
   async upsert_procedures() {
-    await this.runSqlFile('./src/core/sql/graph_create_or_read_word.sql');
-    await this.runSqlFile('./src/core/sql/graph_create_word_sequence.sql');
-    await this.runSqlFile(
-      './src/core/sql/graph_create_or_read_word_sequence.sql',
-    );
-    await this.runSqlFile('./src/core/sql/graph_build_textual_nodes.sql');
-    await this.runSqlFile('./src/core/sql/graph_utils.sql');
-    await this.runSqlFile('./src/core/sql/graph_add_relationship.sql');
-    await this.runSqlFile('./src/core/sql/graph_add_node.sql');
-    await this.runSqlFile('./src/core/sql/graph_create_verse.sql');
-    await this.runSqlFile('./src/core/sql/graph_create_chapter.sql');
-    await this.runSqlFile('./src/core/sql/graph_create_book.sql');
-    await this.runSqlFile('./src/core/sql/graph_create_bible.sql');
+    for (const path of sqlFilePaths) {
+      await this.runSqlFile(path);
+    }
   }
 
   async runSqlFile(path: string) {
     console.log('loading SQL:', path);
     const data = readFileSync(path, 'utf8');
-    const res = await this.pg.pool.query(data, []);
+    await this.pg.pool.query(data, []);
   }
 }
